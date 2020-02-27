7eec3ffd14a02993d7f40c7d06b755a254d34fe6
ahmedfarag9 <ahmedfarag.a95@gmail.com>
2020-02-27 18:39:25 +0200

    Added some style containers

--- App.js
+++ App.js
@@ -2,3 +2,12 @@ import React from 'react';
 import {View, Button, Text, StyleSheet} from 'react-native'
 import {Constants} from 'expo'
 
+const styles = StyleSheet.create({
+	clockContainer:{
+		flexDirection: 'row',
+		alignItems: 'center'
+	},
+	appContainer: {
+		paddingTop: Constants.statusBarHight,
+	}
+})

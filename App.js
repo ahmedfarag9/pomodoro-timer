39af78e4d47337a7af5a8b73dfe9c3b577ec7593
ahmedfarag9 <ahmedfarag.a95@gmail.com>
2020-02-28 18:06:04 +0200

    Added some variables

--- App.js
+++ App.js
@@ -2,12 +2,22 @@ import React from 'react';
 import {View, Button, Text, StyleSheet} from 'react-native'
 import {Constants} from 'expo'
 
+let work_time = "25"
+let break_time = "5"
+let work_time_text = "WORK TIMER"
+let break_time_text = "BREAK TIMER"
+let start_state_text = "START"
+let pause_state_text = "PAUSE"
+
+
 const styles = StyleSheet.create({
 	clockContainer:{
 		flexDirection: 'row',
 		alignItems: 'center'
 	},
 	appContainer: {
-		paddingTop: Constants.statusBarHight,
+		//paddingTop: Constants.statusBarHight,
 	}
 })
+
+

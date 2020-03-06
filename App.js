ba19cacbc6fe4700c72543b2be9f01a33b4b7fae
ahmedfarag9 <ahmedfarag.a95@gmail.com>
2020-03-06 19:57:08 +0200

    Added style for buttons

--- App.js
+++ App.js
@@ -23,7 +23,16 @@ const styles = StyleSheet.create({
         borderColor: '#7a42f4',
         borderWidth: 1
     },
+    submitButton: {
+		backgroundColor: '#7a42f4',
+		padding: 10,
+		margin: 15,
+		height: 40,
+    },
+    submitButtonText:{
+		color: 'white'
 	}
+
 	/*appContainer: {
 		//paddingTop: Constants.statusBarHight,
 	}*/

6f1a59716925c0b6a1ebbad2bcbabff5c784ba8b
ahmedfarag9 <ahmedfarag.a95@gmail.com>
2020-03-16 18:31:01 +0200

    Added some comments

--- App.js
+++ App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import {View, Button, Text, StyleSheet, TouchableOpacity, TextInput, ImageBackground} from 'react-native'
 import {Vibrate} from './utils/vibrate.js'
 
-
+// fixed variables
 let work_time = (25 * 60 * 1000)
 let break_time = (5 * 60 * 1000)
 let work_time_text = "WORK TIMER"
@@ -10,6 +10,7 @@ let break_time_text = "BREAK TIMER"
 let start_state_text = "START"
 let pause_state_text = "PAUSE"
 
+// style components
 const styles = StyleSheet.create({
 	clockContainer:{
 		flexDirection: 'row',
